276156dd0cb0dcbc69df33d98b116e7091094076
HungNg <21130367@st.hcmuaf.edu.vn>
2025-04-26 22:12:03 +0700

    get dependency http and json

---
+++ ConvertApp/app/src/main/java/com/nlu/convertapp/api/ApiService.java
@@ -0,0 +1,15 @@
+package com.nlu.convertapp.api;
+
+import com.nlu.convertapp.models.TextToSpeechRequest;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+}
